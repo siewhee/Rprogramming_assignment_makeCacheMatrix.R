4b6afb3c31f3983fcc4fc39383e0579a33c680a1
siewhee <69128426+siewhee@users.noreply.github.com>
2020-08-24 09:12:22 +0800

    Add files via upload

---
+++ Rprogramming_week3_lesson_assignment.R
@@ -0,0 +1,26 @@
+## w3 r programming assignment 
+
+makeCacheMatrix <- function(x =matrix()){
+  inver  <- NULL
+  set    <- function(y){
+    x     <<- y
+    inver <<- NULL
+  }
+  get <- function(){x} 
+  setInverse <- function(inverse) {inver <<- inverse}
+  getInverse <- function(){inver}
+  list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
+}
+
+cacheSolve <- function(x, ...){
+  inver <- x$getInverse()
+  if(!is.null(inver)) {
+    message("getting cached data")
+    return(inver)
+  }
+  mat   <- x$get()
+  inver <- solve(mat, ...)  
+  x$setInverse(inver)
+  inver
+}
+
